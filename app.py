47c0f34d74fdc1c638028e88bfbd92876cd38e4b
Jake Shin <yellowman2@naver.com>
2020-10-30 15:27:41 +0900

    1030
    Flask error fixed

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
 
 
@@ -14,17 +13,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 dbfile = os.path.join(basedir,"db.sqlite")
 
-conn =sqlite3.connect('/home/mango/Downloads/chadwick.db')
-print("Opened successfully")
-
-cursor = conn.execute("select yearID from Salaries Limit 10")
-
-for row in cursor:
-    print("YearID = ", row[0])
-
 app = Flask(__name__)
 
-app.config['SQLALCHEMY+DATABASE_URI']= 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + dbfile
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -33,7 +24,7 @@ db = SQLAlchemy(app)
 class Test(db.Model):
     __tablename__ = 'test_table'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.string(32), unique = True)
+    name = db.Column(db.String(32), unique = True)
 
 db.create_all()
 
